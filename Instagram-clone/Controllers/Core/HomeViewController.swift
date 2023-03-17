fd173d445514cece995c7ae2e3d77f429ddf8328
Tsenguun <Tsenguun.batbold12@gmail.com>
2023-03-17 22:45:49 +0100

    2.1 Set up collection view for single post

--- Instagram-clone/Controllers/Core/HomeViewController.swift
+++ Instagram-clone/Controllers/Core/HomeViewController.swift
@@ -6,16 +6,91 @@
 //
 
 import UIKit
+import SnapKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    private var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "Instagram"
         view.backgroundColor = .systemBackground
+        
+        configureCollectionView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        collectionView?.snp.makeConstraints { make in
+            make.width.height.equalToSuperview()
+        }
     }
 
+    func configureCollectionView() {
+        // 60 + 40 + 40 + 60 + 40
+        let sectionHeight: CGFloat = 240 + view.width
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { index, _ in
+            
+            
+            // cell for poster
+            let posterItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60)))
+            
+            // bigger cell for post
+            let postItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1)))
+            
+            // cell with actions
+            let actionsItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(40)))
+            
+            // like count cell
+            let likeCountItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(40)))
+
+            // timestamp cell
+            let captionItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60)))
+            
+            let timeStampItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(40)))
+
+            
+            // Group
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(sectionHeight)), subitems: [posterItem, postItem, actionsItem, likeCountItem, captionItem, timeStampItem])
+            
+            // Sections
+            let section = NSCollectionLayoutSection(group: group)
+            
+            section.contentInsets = NSDirectionalEdgeInsets(top: 3, leading: 0, bottom: 10, trailing: 0)
+            
+            return section
+        }))
+
+        
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        self.collectionView = collectionView
+    }
+    
+    // CollectionView
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.contentView.backgroundColor = UIColor.random
+        
+        return cell
+    }
 
 }
 
--- Instagram-clone/Resources/Extensions.swift
+++ Instagram-clone/Resources/Extensions.swift
@@ -52,3 +52,12 @@ extension Encodable {
         return json
     }
 }
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(red: .random(in: 0.4...1),
+                       green: .random(in: 0.4...1),
+                       blue: .random(in: 0.4...1),
+                       alpha: 1)
+    }
+}
