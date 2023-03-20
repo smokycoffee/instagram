f0e696d30c4172226fc4cd111c0dae27818b2ae9
Tsenguun <Tsenguun.batbold12@gmail.com>
2023-03-20 12:52:06 +0100

    3.3: part 1, filtering configured

--- Instagram-clone/Controllers/Other/PostEditViewController.swift
+++ Instagram-clone/Controllers/Other/PostEditViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import CoreImage
 
 class PostEditViewController: UIViewController {
     
@@ -37,7 +38,12 @@ class PostEditViewController: UIViewController {
         title = "Edit"
         imageView.image = image
         view.addSubview(imageView)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+4, execute: { // dummy to test
+            self.filterImage(image: self.image)
+        })
     }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         imageView.snp.makeConstraints { make in
@@ -46,5 +52,22 @@ class PostEditViewController: UIViewController {
         }
     }
 
+    private func filterImage(image: UIImage) {
+        guard let cgImage = image.cgImage else {return}
+        let filter = CIFilter(name: "CIColorMonochrome")
+        filter?.setValue(CIImage(cgImage: cgImage), forKey: "inputImage")
+        filter?.setValue(CIColor(red: 0.7, green: 0.7, blue: 0.7), forKey: "inputColor")
+        filter?.setValue(1.0, forKey: "inputIntensity")
+        
+        guard let outputImage = filter?.outputImage else {return}
+        
+        let context = CIContext()
+        
+        if let outputcgImage =  context.createCGImage(outputImage, from: outputImage.extent) {
+            let filteredImage = UIImage(cgImage: outputcgImage)
+            
+            imageView.image = filteredImage
+        }
+    }
 
 }
